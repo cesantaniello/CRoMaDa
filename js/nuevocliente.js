5af8cb2b02d2a7533cdca00711c33046ef340dfc
Administrator <admin@example.com>
2021-09-27 12:46:51 +0200

    3. Agrega clientes a la BBDD

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -36,6 +36,40 @@
 
             return;
         }
+
+        // Crear un objeto con la información
+        const cliente = {
+            nombre, 
+            email, 
+            telefono, 
+            empresa
+        }
+
+        cliente.id = Date.now();
+
+        crearNuevoCliente(cliente);
+    }
+
+    function crearNuevoCliente(cliente) {
+        const transaction = DB.transaction(['crm'], 'readwrite');
+
+        const objectStore = transaction.objectStore('crm');
+
+        objectStore.add(cliente);
+
+        transaction.onerror = function() {
+            imprimirAlerta('Hubo un error', 'error');
+        }
+
+        transaction.oncomplete = function() {
+            console.log('Cliente agregado');
+
+            imprimirAlerta('El cliente se agregó correctamente');
+
+            setTimeout(() => {
+                window.location.href = 'index.html';
+            }, 3000);
+        }
     }
     
     function imprimirAlerta(mensaje, tipo) {
